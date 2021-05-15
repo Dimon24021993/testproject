c01464f39c94220dd34130cf697ef134641f4461
Vetal <Vetal@DESKTOP-SE0KN5U>
2021-05-15 13:35:12 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" height="100" dark>
+      <v-col cols="12">
+        <v-row justify="center">
+          <v-text-field
+            label="NPM package name"
+            placeholder="NPM package name"
+            solo
+            hide-details
+            light
+            flat
+            dense
+          ></v-text-field>
+        </v-row>
+      </v-col>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <!-- <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+      <v-col></v-col> -->
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    info: [],
+  }),
+  created() {
+    axios
+      .get("http://registry.npmjs.com/-/v1/search?text=vuex")
+      .then((res) => window.console.log(res.data.objects));
+  },
+};
+</script>
